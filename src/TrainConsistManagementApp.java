2629d1e3733af9757ae3277d90f6d0fa7f0bbd40
Manaswini-2006 <sb6468@srmist.edu.in>
2026-04-06 16:00:43 +0530

    UC19:Binary Search for Bogie ID (Optimized Searching)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -2,19 +2,29 @@ public class TrainConsistManagementApp {
     public static void main(String[] args) {
         String[] bogies = {"BG101","BG205","BG309","BG412","BG550"};
 
-        System.out.println(search(bogies, "BG309"));
-        System.out.println(search(bogies, "BG999"));
-        System.out.println(search(bogies, "BG101"));
-        System.out.println(search(bogies, "BG550"));
+        System.out.println(binarySearch(bogies, "BG309"));
+        System.out.println(binarySearch(bogies, "BG999"));
+        System.out.println(binarySearch(bogies, "BG101"));
+        System.out.println(binarySearch(bogies, "BG550"));
 
         String[] single = {"BG101"};
-        System.out.println(search(single, "BG101"));
+        System.out.println(binarySearch(single, "BG101"));
     }
 
-    public static boolean search(String[] arr, String key) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].equals(key)) {
+    public static boolean binarySearch(String[] arr, String key) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int result = key.compareTo(arr[mid]);
+
+            if (result == 0) {
                 return true;
+            } else if (result < 0) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
         return false;
