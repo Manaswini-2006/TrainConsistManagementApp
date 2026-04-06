d4e7e668af521608edd25fbb5d774ee5dccf8414
Manaswini-2006 <sb6468@srmist.edu.in>
2026-04-06 15:56:20 +0530

    UC17:Sort Bogie Names Using Arrays.sort()

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,40 +1,25 @@
 import java.util.Arrays;
 
-class PassengerBogieSorter {
-    public static void bubbleSort(int[] capacities) {
-        int n = capacities.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (capacities[j] > capacities[j + 1]) {
-                    int temp = capacities[j];
-                    capacities[j] = capacities[j + 1];
-                    capacities[j + 1] = temp;
-                }
-            }
-        }
-    }
-}
-
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
-        int[] capacities1 = {72, 56, 24, 70, 60};
-        PassengerBogieSorter.bubbleSort(capacities1);
-        System.out.println(Arrays.toString(capacities1));
+        String[] b1 = {"Sleeper","AC Chair","First Class","General","Luxury"};
+        Arrays.sort(b1);
+        System.out.println(Arrays.toString(b1));
 
-        int[] capacities2 = {24, 56, 60, 70, 72};
-        PassengerBogieSorter.bubbleSort(capacities2);
-        System.out.println(Arrays.toString(capacities2));
+        String[] b2 = {"Luxury","General","Sleeper","AC Chair"};
+        Arrays.sort(b2);
+        System.out.println(Arrays.toString(b2));
 
-        int[] capacities3 = {72, 56, 56, 24};
-        PassengerBogieSorter.bubbleSort(capacities3);
-        System.out.println(Arrays.toString(capacities3));
+        String[] b3 = {"AC Chair","First Class","General"};
+        Arrays.sort(b3);
+        System.out.println(Arrays.toString(b3));
 
-        int[] capacities4 = {50};
-        PassengerBogieSorter.bubbleSort(capacities4);
-        System.out.println(Arrays.toString(capacities4));
+        String[] b4 = {"Sleeper","AC Chair","Sleeper","General"};
+        Arrays.sort(b4);
+        System.out.println(Arrays.toString(b4));
 
-        int[] capacities5 = {40, 40, 40};
-        PassengerBogieSorter.bubbleSort(capacities5);
-        System.out.println(Arrays.toString(capacities5));
+        String[] b5 = {"Sleeper"};
+        Arrays.sort(b5);
+        System.out.println(Arrays.toString(b5));
     }
 }
