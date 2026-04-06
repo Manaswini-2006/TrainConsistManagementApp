4571e78cf6f9cf63225838f9c646a7cec2fce380
Manaswini-2006 <sb6468@srmist.edu.in>
2026-04-06 15:33:14 +0530

    UC16:Sort Passenger Bogies by Capacity

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,48 +1,40 @@
-import java.util.*;
+import java.util.Arrays;
 
-class CargoSafetyException extends RuntimeException {
-    public CargoSafetyException(String message) {
-        super(message);
-    }
-}
-
-class GoodsBogie {
-    private String type;
-    private String cargo;
-
-    public GoodsBogie(String type) {
-        this.type = type;
-    }
-
-    public void assignCargo(String cargo) {
-        try {
-            if (type.equalsIgnoreCase("Rectangular") && cargo.equalsIgnoreCase("Petroleum")) {
-                throw new CargoSafetyException("Unsafe cargo assignment");
+class PassengerBogieSorter {
+    public static void bubbleSort(int[] capacities) {
+        int n = capacities.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (capacities[j] > capacities[j + 1]) {
+                    int temp = capacities[j];
+                    capacities[j] = capacities[j + 1];
+                    capacities[j + 1] = temp;
+                }
             }
-            this.cargo = cargo;
-            System.out.println(type + " bogie assigned with cargo: " + cargo);
-        } catch (CargoSafetyException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            System.out.println("Assignment attempt completed for " + type + " bogie");
         }
-    }
-
-    public String getCargo() {
-        return cargo;
     }
 }
 
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
+        int[] capacities1 = {72, 56, 24, 70, 60};
+        PassengerBogieSorter.bubbleSort(capacities1);
+        System.out.println(Arrays.toString(capacities1));
 
-        GoodsBogie b1 = new GoodsBogie("Cylindrical");
-        GoodsBogie b2 = new GoodsBogie("Rectangular");
+        int[] capacities2 = {24, 56, 60, 70, 72};
+        PassengerBogieSorter.bubbleSort(capacities2);
+        System.out.println(Arrays.toString(capacities2));
 
-        b1.assignCargo("Petroleum");
-        b2.assignCargo("Coal");
-        b2.assignCargo("Petroleum");
+        int[] capacities3 = {72, 56, 56, 24};
+        PassengerBogieSorter.bubbleSort(capacities3);
+        System.out.println(Arrays.toString(capacities3));
 
-        System.out.println("Program continues...");
+        int[] capacities4 = {50};
+        PassengerBogieSorter.bubbleSort(capacities4);
+        System.out.println(Arrays.toString(capacities4));
+
+        int[] capacities5 = {40, 40, 40};
+        PassengerBogieSorter.bubbleSort(capacities5);
+        System.out.println(Arrays.toString(capacities5));
     }
 }
