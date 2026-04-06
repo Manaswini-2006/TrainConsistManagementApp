33f34f57f38b4ad7fa0392be0ffa1ccdfa761e94
Manaswini-2006 <sb6468@srmist.edu.in>
2026-04-06 15:58:24 +0530

    UC18:Linear Search for Bogie ID (Array-Based Searching)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,25 +1,22 @@
-import java.util.Arrays;
-
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
-        String[] b1 = {"Sleeper","AC Chair","First Class","General","Luxury"};
-        Arrays.sort(b1);
-        System.out.println(Arrays.toString(b1));
+        String[] bogies = {"BG101","BG205","BG309","BG412","BG550"};
 
-        String[] b2 = {"Luxury","General","Sleeper","AC Chair"};
-        Arrays.sort(b2);
-        System.out.println(Arrays.toString(b2));
+        System.out.println(search(bogies, "BG309"));
+        System.out.println(search(bogies, "BG999"));
+        System.out.println(search(bogies, "BG101"));
+        System.out.println(search(bogies, "BG550"));
 
-        String[] b3 = {"AC Chair","First Class","General"};
-        Arrays.sort(b3);
-        System.out.println(Arrays.toString(b3));
+        String[] single = {"BG101"};
+        System.out.println(search(single, "BG101"));
+    }
 
-        String[] b4 = {"Sleeper","AC Chair","Sleeper","General"};
-        Arrays.sort(b4);
-        System.out.println(Arrays.toString(b4));
-
-        String[] b5 = {"Sleeper"};
-        Arrays.sort(b5);
-        System.out.println(Arrays.toString(b5));
+    public static boolean search(String[] arr, String key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(key)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
